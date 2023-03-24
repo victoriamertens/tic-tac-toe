caeb1da66bbbd03df0a9868431d20874d62e26ba
Victoria Mertens <victoria.mertens44@gmail.com>
2023-03-24 16:00:50 -0500

    Structure for tic-tac-toe complete

--- src/Components/Board.jsx
+++ src/Components/Board.jsx
@@ -1,18 +1,36 @@
 import React from 'react';
-import { Box } from 'src/Components/Box.jsx';
+import '/Users/victoriamertens/Documents/Prime Academy Docs /Graduation-Projects/tic-tac-toe-proj/src/Components/Board.css';
 
 export function Board() {
   return (
-    <>
-      <Box id="1" />
-      <Box id="2" />
-      <Box id="3" />
-      <Box id="4" />
-      <Box id="5" />
-      <Box id="6" />
-      <Box id="7" />
-      <Box id="8" />
-      <Box id="9" />
-    </>
+    <div class="container">
+      <div id="box1" class="box">
+        Box 1
+      </div>
+      <div id="box2" class="box">
+        Box 2
+      </div>
+      <div id="box3" class="box">
+        Box 3
+      </div>
+      <div id="box4" class="box">
+        Box 4
+      </div>
+      <div id="box5" class="box">
+        Box 5
+      </div>
+      <div id="box6" class="box">
+        Box 6
+      </div>
+      <div id="box7" class="box">
+        Box 7
+      </div>
+      <div id="box8" class="box">
+        Box 8
+      </div>
+      <div id="box9" class="box">
+        Box 9
+      </div>
+    </div>
   );
 }
--- src/Components/Box.jsx
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-export function Box(props) {
-  return <div id={props.id}> Box {props.id}</div>;
-}
