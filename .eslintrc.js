2df2832a0f85b1bb2808ad8ac6502be309cd3d52
Victoria Mertens <victoria.mertens44@gmail.com>
2023-03-27 12:29:48 -0500

    Added linting to project.

---
+++ .eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+  ],
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['react', 'react-hooks'],
+  ignorePatterns: ['.eslintrc.js', 'webpack.config.js'],
+  rules: {
+    'react/prop-types': 'off',
+  },
+};
